245a072b74055d8632372120575cdf06d3009a21
Soto <sotosade@amazon.com>
2026-05-26 17:06:11 -0600

    fix: cardImages script

--- src/utils/cardImages.js
+++ src/utils/cardImages.js
@@ -1,102 +1,166 @@
+// Import all images statically at the top
+import adrianaCruz from '../images/cards/adriana-cruz.png';
+import jeffBezos from '../images/cards/jeff-bezos.png';
+import jp from '../images/cards/jp.png';
+import amazonSjo16 from '../images/cards/amazon-sjo-16.png';
+import andreySoto from '../images/cards/andrey-soto.png';
+import rafa from '../images/cards/rafa.png';
+import ireVargas from '../images/cards/ire-vargas.png';
+import pawel from '../images/cards/pawel.png';
+import ismaVindas from '../images/cards/isma-vindas.png';
+import arielBolanos from '../images/cards/ariel-bolanos.png';
+import eimyTorres from '../images/cards/eimy-torres.png';
+import marypazMora from '../images/cards/marypaz-mora.png';
+import darioCampos from '../images/cards/dario-campos.png';
+import julianHernandez from '../images/cards/julian-hernandez.png';
+import stevenGonzalez from '../images/cards/steven-gonzalez.png';
+import danielGuadamuz from '../images/cards/daniel-guadamuz.png';
+import jorgeBrenes from '../images/cards/jorge-brenes.png';
+import nickNova from '../images/cards/nick-nova.png';
+import veroDuran from '../images/cards/vero-duran.png';
+import joseReyes from '../images/cards/jose-reyes.png';
+import pauletteRuiz from '../images/cards/paulette-ruiz.png';
+import joseMonge from '../images/cards/jose-monge.png';
+import nathalieGonzalez from '../images/cards/nathalie-gonzalez.png';
+import brayanSalazar from '../images/cards/brayan-salazar.png';
+import yanifelAlvarado from '../images/cards/yanifel-alvarado.png';
+import valeriaCubero from '../images/cards/valeria-cubero.png';
+import antonioAmez from '../images/cards/antonio-amez.png';
+import greivinMeza from '../images/cards/greivin-meza.png';
+import paolaJimenez from '../images/cards/paola-jimenez.png';
+import nataliaAbarca from '../images/cards/natalia-abarca.png';
+import isaacAvila from '../images/cards/isaac-avila.png';
+import andreinaCastillo from '../images/cards/andreina-castillo.png';
+import wilbertCedeno from '../images/cards/wilbert-cedeno.png';
+import raquelFernandez from '../images/cards/raquel-fernandez.png';
+import fergieMoncada from '../images/cards/fergie-moncada.png';
+import tomasSoley from '../images/cards/tomas-soley.png';
+import marianellaPerez from '../images/cards/marianella-perez.png';
+import allanHidalgo from '../images/cards/allan-hidalgo.png';
+import katherineMorera from '../images/cards/katherine-morera.png';
+import danielaJimenez from '../images/cards/daniela-jimenez.png';
+import pilarHernandez from '../images/cards/pilar-hernandez.png';
+import maikolDiaz from '../images/cards/maikol-diaz.png';
+import raquelBlanco from '../images/cards/raquel-blanco.png';
+import tayronDuran from '../images/cards/tayron-duran.png';
+import stephAngulo from '../images/cards/steph-angulo.png';
+import gabrielaGomez from '../images/cards/gabriela-gomez.png';
+import ramiroChacon from '../images/cards/ramiro-chacon.png';
+import henryRodriguez from '../images/cards/henry-rodriguez.png';
+import nataliaBlanco from '../images/cards/natalia-blanco.png';
+import yeylanAguirre from '../images/cards/yeylan-aguirre.png';
+import lyAnnHerrera from '../images/cards/ly-ann-herrera.png';
+import pabloDelgado from '../images/cards/pablo-delgado.png';
+import davidGonzalez from '../images/cards/david-gonzalez.png';
+import arianaAlcazar from '../images/cards/ariana-alcazar.png';
+import paolaQuiros from '../images/cards/paola-quiros.png';
+import sharonUmana from '../images/cards/sharon-umana.png';
+import davidLoaiza from '../images/cards/david-loaiza.png';
+import lauraMunoz from '../images/cards/laura-munoz.png';
+import henryMolina from '../images/cards/henry-molina.png';
+import estebanValerio from '../images/cards/esteban-valerio.png';
+import jensiPeinado from '../images/cards/jensi-peinado.png';
+import davidSeitz from '../images/cards/david-seitz.png';
+import jazminDelgado from '../images/cards/jazmin-delgado.png';
+import joseMasis from '../images/cards/jose-masis.png';
+import mariaPana from '../images/cards/maria-pana.png';
+import jordanMora from '../images/cards/jordan-mora.png';
+import sharonQuesada from '../images/cards/sharon-quesada.png';
+import mitzyHernandez from '../images/cards/mitzy-hernandez.png';
+import kevinHidalgo from '../images/cards/kevin-hidalgo.png';
+import maycolMoreira from '../images/cards/maycol-moreira.png';
+import andyJassy from '../images/cards/andy-jassy.png';
+
+// Static mapping object
+const imageMap = {
+  'ADRIANA CRUZ': adrianaCruz,
+  'JEFF BEZOS': jeffBezos,
+  'JP': jp,
+  'SJO 16 AFZ': amazonSjo16,
+  'ANDREY SOTO': andreySoto,
+  'RAFA': rafa,
+  'IRE': ireVargas,
+  'PAWEL': pawel,
+  'ISMA VINDAS': ismaVindas,
+  'ARIEL BOLANOS': arielBolanos,
+  'EIMY TORRES': eimyTorres,
+  'MARYPAZ MORA': marypazMora,
+  'DARIO CAMPOS': darioCampos,
+  'JULIAN HERNANDEZ': julianHernandez,
+  'STEVEN GONZALEZ': stevenGonzalez,
+  'DANIEL GUADAMUZ': danielGuadamuz,
+  'JORGE BRENES': jorgeBrenes,
+  'NICK NOVA': nickNova,
+  'VERO DURAN': veroDuran,
+  'JOSE REYES': joseReyes,
+  'PAULETTE RUIZ': pauletteRuiz,
+  'JOSE MONGE': joseMonge,
+  'NATHALIE GONZALEZ': nathalieGonzalez,
+  'BRAYAN SALAZAR': brayanSalazar,
+  'YANIFEL ALVARADO': yanifelAlvarado,
+  'VALERIA CUBERO': valeriaCubero,
+  'ANTONIO AMEZ': antonioAmez,
+  'GREIVIN MEZA': greivinMeza,
+  'PAOLA JIMENEZ': paolaJimenez,
+  'NATALIA ABARCA': nataliaAbarca,
+  'ISAAC AVILA': isaacAvila,
+  'ANDREINA CASTILLO': andreinaCastillo,
+  'WILBERT CEDENO': wilbertCedeno,
+  'RAQUEL FERNANDEZ': raquelFernandez,
+  'FERGIE MONCADA': fergieMoncada,
+  'TOMAS SOLEY': tomasSoley,
+  'MARIANELLA PEREZ': marianellaPerez,
+  'ALLAN HIDALGO': allanHidalgo,
+  'KATHERINE MORERA': katherineMorera,
+  'DANIELA JIMENEZ': danielaJimenez,
+  'PILAR HERNANDEZ': pilarHernandez,
+  'MAIKOL DIAZ': maikolDiaz,
+  'RAQUEL BLANCO': raquelBlanco,
+  'TAYRON DURAN': tayronDuran,
+  'STEPH ANGULO': stephAngulo,
+  'GABRIELA GOMEZ': gabrielaGomez,
+  'RAMIRO CHACON': ramiroChacon,
+  'HENRY RODRIGUEZ': henryRodriguez,
+  'NATALIA BLANCO': nataliaBlanco,
+  'YEYLAN AGUIRRE': yeylanAguirre,
+  'LY ANN HERRERA': lyAnnHerrera,
+  'PABLO DELGADO': pabloDelgado,
+  'DAVID GONZALEZ': davidGonzalez,
+  'ARIANA ALCAZAR': arianaAlcazar,
+  'PAOLA QUIROS': paolaQuiros,
+  'SHARON UMANA': sharonUmana,
+  'DAVID LOAIZA': davidLoaiza,
+  'LAURA MUNOZ': lauraMunoz,
+  'HENRY MOLINA': henryMolina,
+  'ESTEBAN VALERIO': estebanValerio,
+  'JENSI PEINADO': jensiPeinado,
+  'DAVID SEITZ': davidSeitz,
+  'JAZMIN DELGADO': jazminDelgado,
+  'JOSE MASIS': joseMasis,
+  'MARIA PANA': mariaPana,
+  'JORDAN MORA': jordanMora,
+  'SHARON QUESADA': sharonQuesada,
+  'MITZY HERNANDEZ': mitzyHernandez,
+  'KEVIN HIDALGO': kevinHidalgo,
+  'MAYCOL MOREIRA': maycolMoreira,
+  'ANDY JASSY': andyJassy
+};
+
 // Helper function to get card image by name
 export const getCardImage = (cardName) => {
   console.log('=== getCardImage DEBUG ===');
   console.log('Input cardName:', cardName);
-  console.log('Uppercase:', cardName.toUpperCase());
 
-  // Convert database name to filename
-  const nameToFilename = {
-    'ADRIANA CRUZ': 'adriana-cruz',
-    'JEFF BEZOS': 'jeff-bezos', 
-    'JP': 'jp',
-    'SJO 16 AFZ': 'amazon-sjo-16',
-    'ANDREY SOTO': 'andrey-soto',
-    'RAFA': 'rafa', // REMOVED: 'RAFA AGUERO': 'rafa',
-    'IRE': 'ire-vargas',
-    'PAWEL': 'pawel', // REMOVED: 'PAWEL PUDLIK': 'pawel',
-    'ISMA VINDAS': 'isma-vindas',
-    'ARIEL BOLANOS': 'ariel-bolanos',
-    'EIMY TORRES': 'eimy-torres',
-    'MARYPAZ MORA': 'marypaz-mora',
-    'DARIO CAMPOS': 'dario-campos',
-    'JULIAN HERNANDEZ': 'julian-hernandez',
-    'STEVEN GONZALEZ': 'steven-gonzalez',
-    'DANIEL GUADAMUZ': 'daniel-guadamuz',
-    'JORGE BRENES': 'jorge-brenes',
-    'NICK NOVA': 'nick-nova',
-    'VERO DURAN': 'vero-duran',
-    'JOSE REYES': 'jose-reyes',
-    'PAULETTE RUIZ': 'paulette-ruiz',
-    'JOSE MONGE': 'jose-monge',
-    'NATHALIE GONZALEZ': 'nathalie-gonzalez',
-    'BRAYAN SALAZAR': 'brayan-salazar',
-    'YANIFEL ALVARADO': 'yanifel-alvarado',
-    'VALERIA CUBERO': 'valeria-cubero',
-    'ANTONIO AMEZ': 'antonio-amez',
-    'GREIVIN MEZA': 'greivin-meza',
-    'PAOLA JIMENEZ': 'paola-jimenez',
-    'NATALIA ABARCA': 'natalia-abarca',
-    'ISAAC AVILA': 'isaac-avila',
-    'ANDREINA CASTILLO': 'andreina-castillo',
-    'WILBERT CEDENO': 'wilbert-cedeno',
-    'RAQUEL FERNANDEZ': 'raquel-fernandez',
-    'FERGIE MONCADA': 'fergie-moncada',
-    'TOMAS SOLEY': 'tomas-soley',
-    'MARIANELLA PEREZ': 'marianella-perez',
-    'ALLAN HIDALGO': 'allan-hidalgo',
-    'KATHERINE MORERA': 'katherine-morera',
-    'DANIELA JIMENEZ': 'daniela-jimenez',
-    'PILAR HERNANDEZ': 'pilar-hernandez',
-    'MAIKOL DIAZ': 'maikol-diaz',
-    'RAQUEL BLANCO': 'raquel-blanco',
-    'TAYRON DURAN': 'tayron-duran',
-    'STEPH ANGULO': 'steph-angulo',
-    'GABRIELA GOMEZ': 'gabriela-gomez',
-    'RAMIRO CHACON': 'ramiro-chacon',
-    'HENRY RODRIGUEZ': 'henry-rodriguez',
-    'NATALIA BLANCO': 'natalia-blanco',
-    'YEYLAN AGUIRRE': 'yeylan-aguirre',
-    'LY ANN HERRERA': 'ly-ann-herrera',
-    'PABLO DELGADO': 'pablo-delgado',
-    'DAVID GONZALEZ': 'david-gonzalez',
-    'ARIANA ALCAZAR': 'ariana-alcazar',
-    'PAOLA QUIROS': 'paola-quiros',
-    'SHARON UMANA': 'sharon-umana',
-    'DAVID LOAIZA': 'david-loaiza',
-    'LAURA MUNOZ': 'laura-munoz',
-    'HENRY MOLINA': 'henry-molina',
-    'ESTEBAN VALERIO': 'esteban-valerio',
-    'JENSI PEINADO': 'jensi-peinado',
-    'DAVID SEITZ': 'david-seitz',
-    'JAZMIN DELGADO': 'jazmin-delgado',
-    'JOSE MASIS': 'jose-masis',
-    'MARIA PANA': 'maria-pana',
-    'JORDAN MORA': 'jordan-mora',
-    'SHARON QUESADA': 'sharon-quesada',
-    'MITZY HERNANDEZ': 'mitzy-hernandez',
-    'KEVIN HIDALGO': 'kevin-hidalgo',
-    'MAYCOL MOREIRA': 'maycol-moreira',
-    'ANDY JASSY': 'andy-jassy'
-  };
+  const upperCaseName = cardName.toUpperCase();
+  const image = imageMap[upperCaseName];
 
-  const filename = nameToFilename[cardName.toUpperCase()];
-  console.log('Mapped filename:', filename);
-
-  if (!filename) {
-    console.warn(`No filename mapping found for: ${cardName}`);
+  if (!image) {
+    console.warn(`No image found for: ${cardName}`);
     return null;
   }
 
-  try {
-    const imagePath = `../images/cards/${filename}.png`;
-    console.log('Trying to require:', imagePath);
-    const result = require(imagePath);
-    console.log('Require successful:', result);
-    return result;
-  } catch (error) {
-    console.error(`Card image not found: ${filename}.png`, error);
-    return null;
-  }
+  console.log('Image found successfully for:', cardName);
+  return image;
 };
 
 export default getCardImage;
